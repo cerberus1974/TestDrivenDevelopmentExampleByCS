9c6a517404081ac2b75ee7c6975e16bb87ac76b7
t_takano <cerberus1974@gmail.com>
2017-12-24 13:28:11 +0900

    fixup! P30~31

---
+++ TestDrivenDevelopmentPractice/Franc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestDrivenDevelopmentPractice
+{
+    class Franc
+    {
+        private int amount;
+
+        public Franc(int amount)
+        {
+            this.amount = amount;
+        }
+
+        public Franc Times(int multiplier)
+        {
+            return new Franc(amount * multiplier);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var dollar = (Franc)obj;
+            return amount == dollar.amount;
+        }
+    }
+}
